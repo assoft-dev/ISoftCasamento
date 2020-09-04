13ce12eee5652ff846016660ea2c4affaf6df2dc
ASSOFTSERVER <ASSOFTSERVER@DESKTOP-OI9GD14>
2020-09-04 16:07:11 +0100

    Aqui Adiconamos Tudo

---
+++ MobileAppCasamento/MobileAppCasamento/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MobileAppCasamento.Services;
+using MobileAppCasamento.Views;
+
+namespace MobileAppCasamento
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobileAppCasamento/MobileAppCasamento/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobileAppCasamento.Models;
+
+namespace MobileAppCasamento.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
